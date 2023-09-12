ec9c7d25974797cc6a0d75cb509eea6fa612a43e
SoExDobin <a90189829@gmail.com>
2023-09-12 19:49:45 +0900

    update Arts

---
+++ Assets/Scripts/Piece/Piece.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece
+{
+    public static string id;
+    public static int x;
+    public static int y;
+}
